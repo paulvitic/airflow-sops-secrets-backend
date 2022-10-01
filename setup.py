ae96e6e9d0f575e1f48b2b48e07037a144ba2bed
paulvitic <git1212Paul>
2022-10-01 18:22:39 +0200

    setup all required packages

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from distutils.core import setup
 
 setup(name='airflow-secrets-sops',
-      version='0.0.5',
+      version='0.0.6',
       packages=['airflow.sops'],
       package_dir={'': 'src'},
       python_requires='>=3.8',
       install_requires=[
             "apache-airflow==2.2.3",
             "apache-airflow-providers-google==6.2.0",
-            "sops==1.18"
+            "sops==1.18",
+            "markupsafe==2.0.1"
       ])
