940b7b0f2e1f0b31ee964d4eafd0ca6ddc422069
paulvitic <git1212Paul>
2022-10-01 10:36:39 +0200

    simple setup

--- README.md
+++ README.md
@@ -1,12 +1,21 @@
+## Setup
+```shell
+source .env/bin/activate
+pip install -r requirements.txt
+```
+
 ## Build
 
 ```shell
-python -m build -n
+pip install build
+python -m build
 ```
 
 ## Push
+To GCP artifact registry
 ```shell
 pip install twine keyrings.google-artifactregistry-auth wheel
 python setup.py bdist_wheel
-gcloud auth application-default login
+pip config set global.index-url https://_json_key_base64:***gcp_registry_writer_service_account_key***@europe-west1-python.pkg.dev/cm-build/comatch-python/simple/
+python -m twine upload --repository-url https://europe-west1-python.pkg.dev/cm-build/comatch-python/ dist/*
 ```
--- setup.py
+++ setup.py
@@ -1,23 +1,14 @@
-from setuptools import setup, find_packages
+from distutils.core import setup
 
-from airflow_secrets_backend import __version__
-
-setup(
-    name='airflow-secrets-sops',
-    version=__version__,
-    packages=find_packages(),
-    url='',
-    license='MIT',
-    author='Paul Vitic',
-    scripts=[],
-    include_package_data=True,
-    author_email='p.vitic@comatch.com',
-    description='An Airflow custom secrets backend for sops encrypted resources in gcs bucket',
-    install_requires=[
-        "apache-airflow==2.2.3",
-        "google-cloud-storage==1.43.0",
-        "google-cloud-kms==2.10.1",
-        "sops==1.18",
-        "markupsafe==2.0.1",
-    ]
-)
+setup(name='airflow-secrets-sops',
+      version='0.0.1',
+      packages=['airflow_sops'],
+      package_dir={'': 'src'},
+      python_requires='>=3.7',
+      install_requires=[
+          "apache-airflow==2.2.3",
+          "google-cloud-storage>=1.43.0",
+          "google-cloud-kms>=2.10.1",
+          "sops==1.18",
+          "markupsafe==2.0.1",
+      ])
