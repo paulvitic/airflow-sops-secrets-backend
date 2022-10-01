5e7ca74cf7b085acf414e4b85cd4fd8c5286b31b
paulvitic <git1212Paul>
2022-10-01 12:42:49 +0200

    upgrade to python 3.8 change directory structure

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup
 
 setup(name='airflow-secrets-sops',
-      version='0.0.1',
-      packages=['airflow_sops'],
+      version='0.0.2',
+      packages=['airflow.sops'],
       package_dir={'': 'src'},
-      python_requires='>=3.7',
+      python_requires='>=3.8',
       install_requires=[
           "apache-airflow==2.2.3",
           "google-cloud-storage>=1.43.0",
